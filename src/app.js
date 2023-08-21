cec6df0cc6022f2dc3807e545328a972cb428db6
matiasfabrizio22 <matias.fabrizio8@gmail.com>
2023-08-21 12:12:56 -0300

    login y registro

--- src/app.js
+++ src/app.js
@@ -14,10 +14,11 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"views/home.html"));
 });
 
+
 app.get("/register", (req, res) => {
-    res.sendFile(path.join(__dirname,"views/register.html"));
+    res.sendFile(path.join(__dirname, "/register.html"));
 });
 
 app.get("/login", (req, res) => {
-    res.sendFile(path.join(__dirname,"views/login.html"));
+    res.sendFile(path.join(__dirname, "/login.html"));
 });
