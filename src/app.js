eaf58c518683144184714e69c8d1a57fd3f80fc3
matiasfabrizio22 <matias.fabrizio8@gmail.com>
2023-08-21 04:06:53 -0300

    casi terminado

--- src/app.js
+++ src/app.js
@@ -11,5 +11,13 @@ console.log( ` se prendio ${PORT} ` );
 });
 
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"views/home.html"))
+    res.sendFile(path.join(__dirname,"views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname,"views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname,"views/login.html"));
 });
